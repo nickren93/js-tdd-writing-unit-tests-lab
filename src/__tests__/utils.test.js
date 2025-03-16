a4458f58cd03d63502ac5e13124f038e78da85fe
Sitong <nickren93@gmail.com>
2025-03-16 19:58:54 -0400

    complete lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,52 @@
 // Your tests here
+import { isPalindrome } from "../utils";
+
+describe("isPalindrome", () => {
+
+  it("gives users double amount of points if their word is a palindrome", () => {
+    const word = "mom"
+
+    const result = isPalindrome(word)
+
+    expect(result).toBe(true)
+  })
+
+  it("gives users double amount of points if their word is a palindrome", () => {
+    const word = "racecar"
+
+    const result = isPalindrome(word)
+
+    expect(result).toBe(true)
+  })
+
+  it("gives users double amount of points if their word is a palindrome", () => {
+    const word = "car"
+
+    const result = isPalindrome(word)
+
+    expect(result).toBe(false)
+  })
+
+  it("return true for words that are a combination of uppercase and lowercase letters when their word is a palindrome", () => {
+    const word = "rACecaR"
+
+    const result = isPalindrome(word)
+
+    expect(result).toBe(true)
+  })
+
+  it("return false for an empty string", () => {
+    const word = ""
+
+    const result = isPalindrome(word)
+
+    expect(result).toBe(false)
+  })
+
+  test(' throw an error if input has any non-alphabetic characters', () => {
+    const word = '不是'
+    //const result = isPalindrome(word)
+    expect(()=>isPalindrome(word)).toThrow('The word provide is non-alphabetic character! Please try again')
+  })
+  
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,27 @@
 // Your code here
+
+export function isPalindrome(word){
+    const reversedWordArray = []
+
+    for (const char of word) {
+        reversedWordArray.unshift(char)
+    }
+
+    const reversedWord = reversedWordArray.join("")
+    /*
+    if(word == ""){
+        return false
+    }
+    else 
+    */
+    
+    if(word == ""){
+        return false
+    }else if(!(/^[A-Za-z]+$/.test(word))){
+        throw new Error('The word provide is non-alphabetic character! Please try again');
+    } else if(word.toLowerCase() == reversedWord.toLowerCase()){
+        return true
+    }else{
+        return false
+    }
+}
